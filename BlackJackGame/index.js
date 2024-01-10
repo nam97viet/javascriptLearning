28af9e66cdeaf908503a05acc76bd059a62745fc
nam97viet <namhoangong97@gmail.com>
2024-01-10 17:26:39 -0600

    blackjack js

--- BlackJackGame/index.js
+++ BlackJackGame/index.js
@@ -1,23 +1,45 @@
-let firstCard = 10
-let secondCard = 10
-
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
-
+let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+
+let cardsEl = document.getElementById("cards-el")
+
+function getRandomCard(){
+    let randomCard = Math.floor(Math.random() * 10) + 1
+    return randomCard
+}
 
 function startGame() {
-    if (sum < 21) {
-        message = "Do you want to draw a card? 🤗"
-        console.log(message)
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    sumEl.textContent = "Sum :" + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a card?"
     }
     else if (sum === 21) {
-        message = "Congrat!!! You got a black jack 😘"
-        console.log(message)
+        message = "You've got a Blackjack"
     }
     else {
-        message = "You lost!!! 😓"
-        console.log(message)
-        isAlive = false
+        message = "You've out of the game!"
     }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
 }
